623a1752a718ce28974f27bc395b488a4812835c
ZygimantasKoncius <zygimantas@etleap.com>
2023-03-31 18:25:29 +0100

    more refs

--- models/marts/logins_by_user.sql
+++ models/marts/logins_by_user.sql
@@ -1,4 +1,4 @@
 SELECT u.id AS user_id, COUNT(e.external_id) AS logins
-FROM public.user u, public.Website_Events e
+FROM {{ref('stg_mysql_user_info')}} u, {{ref('stg_mongo_web_events')}} e
 WHERE u.external_id = e.external_id
 GROUP BY u.id
--- models/marts/spend_per_user.sql
+++ models/marts/spend_per_user.sql
@@ -1,5 +1,5 @@
 SELECT p.user_id, SUM(i.price) AS spend
-  FROM public.purchase p 
-    INNER JOIN public.line_item li ON li.purchase_id = p.id
-    INNER JOIN public.item i ON i.item_id = li.item_id
+  FROM {{ref('stg_mysql_purchase')}} p 
+    INNER JOIN {{ref('stg_mysql_line_item')}} li ON li.purchase_id = p.id
+    INNER JOIN {{ref('stg_mysql_item')}} i ON i.item_id = li.item_id
   GROUP BY p.user_id
